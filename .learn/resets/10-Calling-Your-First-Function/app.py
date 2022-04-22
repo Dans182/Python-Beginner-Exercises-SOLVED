4d9eb16a13631954463f894bc6b5568f79f72a70
Dans182 <dgaiteiro@gmail.com>
2022-04-22 18:51:40 +0000

    hasta ejercicio 10

--- .learn/resets/10-Calling-Your-First-Function/app.py
+++ .learn/resets/10-Calling-Your-First-Function/app.py
@@ -1,6 +1,6 @@
 def is_odd(my_number):
   	return (my_number % 2 != 0)
 
+
 def my_main_code():
     # your code here
-    print(is_odd(45345))
--- exercises/08.1-How-Much-The-Wedding-Costs/app.py
+++ exercises/08.1-How-Much-The-Wedding-Costs/app.py
@@ -2,5 +2,13 @@ user_input = int(input('How many people are coming to your wedding?\n'))
 
 if user_input <= 50:
     price = 4000
-
-print('Your wedding will cost '+str(price)+' dollars')
+    print('Your wedding will cost '+str(price)+' dollars')
+elif user_input <= 100:
+    price = 10000
+    print('Your wedding will cost '+str(price)+' dollars')
+elif user_input <= 200:
+    price = 15000
+    print('Your wedding will cost '+str(price)+' dollars')
+else:
+    price = 20000
+    print('Your wedding will cost '+str(price)+' dollars')
