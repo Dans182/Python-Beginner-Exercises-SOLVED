6c2d7ba49967b9f4288729c25794b9c0f1eda730
Dans182 <dgaiteiro@gmail.com>
2022-04-23 12:32:02 +0000

    listo

--- exercises/19-Bottles-Of-Milk/app.py
+++ exercises/19-Bottles-Of-Milk/app.py
@@ -1,10 +1,12 @@
 def number_of_bottles():
     for value in range(99, -1, -1):
-        if value == 1: 
-            print(str(value) + " bottle of milk on the wall, " + str(value) + " bottle of milk. Take one down and pass it around, no more bottles of milk on the wall")
+        if value == 2: 
+            print(str(value) + " bottles of milk on the wall, " + str(value) + " bottles of milk. Take one down and pass it around, " + str(value - 1) + " bottle of milk on the wall.")
+        elif value == 1: 
+            print(str(value) + " bottle of milk on the wall, " + str(value) + " bottle of milk. Take one down and pass it around, no more bottles of milk on the wall.")
         elif value == 0: 
-            print("No more bottles of milk on the wall, no more bottles of milk. Go to the store and buy some more, 99 bottles of milk on the wall")
+            print("No more bottles of milk on the wall, no more bottles of milk. Go to the store and buy some more, 99 bottles of milk on the wall.")
         else:
-            print(str(value) + " bottles of milk on the wall, " + str(value) + " bottles of milk. Take one down and pass it around, " + str(value - 1) + " bottles of milk on the wall")
+            print(str(value) + " bottles of milk on the wall, " + str(value) + " bottles of milk. Take one down and pass it around, " + str(value - 1) + " bottles of milk on the wall.")
 
-number_of_bottles()
+print(number_of_bottles())
