2b9b9c74cf40d83dc223c735c7867bb9fbfd3c5b
Dans182 <dgaiteiro@gmail.com>
2022-04-23 10:21:38 +0000

    15

--- exercises/15-Looping-With-FizzBuzz/app.py
+++ exercises/15-Looping-With-FizzBuzz/app.py
@@ -1,5 +1,12 @@
 def fizz_buzz():
     # your code here
-
-
+    for value in range (1,101):
+        if value % 15 == 0: 
+            print("FizzBuzz")
+        elif value % 3 == 0: 
+            print("Fizz")
+        elif value % 5 == 0:
+            print("Buzz")
+        else: 
+            print(value)
 fizz_buzz()
